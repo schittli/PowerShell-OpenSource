500d97c7beac8fd9a2ad04c9d60766fc17d41081
Tom-- <github@jig.ch>
2022-11-28 22:27:36 +0100

    cmd zum Start zugefügt

---
+++ Functions/Setup-CiscoAnyConnect/Update-CiscoAnyConnect.cmd
@@ -0,0 +1,11 @@
+@Echo Off
+Rem Aktualsiert Cisco AnyConnect
+Rem holt die MSI von:
+Rem https://www.akros.ch/it/Cisco/AnyConnect/Windows/PowerShell/Bin/
+Rem 221123, Tom
+
+Rem Mit Debug Infos
+Rem powershell.exe -NoProfile -ExecutionPolicy Bypass -NoExit -Command CD 'C:\Temp'; Write-Host "";$CommandLineArgs = [System.Environment]::GetCommandLineArgs();Write-Host "";Write-Host 'GetCommandLineArgs() -join:' -ForegroundColor Yellow;Write-Host ($CommandLineArgs -join ' ') ;Write-Host "";[Net.ServicePointManager]::SecurityProtocol = """Tls12"""; Invoke-Expression """ &{ $(Invoke-RestMethod -DisableKeepAlive -Uri 'https://github.com/schittli/PowerShell-OpenSource/raw/main/Functions/Setup-CiscoAnyConnect/Setup-CiscoAnyConnect.ps1') } -InstallNosergroupDefaultModules -InstallFromWeb -ShowDebugInfos"""
+
+powershell.exe -NoProfile -ExecutionPolicy Bypass -NoExit -Command CD 'C:\Temp'; Write-Host "";[Net.ServicePointManager]::SecurityProtocol = """Tls12"""; Invoke-Expression """ &{ $(Invoke-RestMethod -DisableKeepAlive -Uri 'https://github.com/schittli/PowerShell-OpenSource/raw/main/Functions/Setup-CiscoAnyConnect/Setup-CiscoAnyConnect.ps1') } -InstallNosergroupDefaultModules -InstallFromWeb -ShowDebugInfos"""
+
