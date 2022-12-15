cc44eb42e97eb9af8bca8340f0b054c185f1113f
Tom-- <github@jig.ch>
2022-12-15 20:17:26 +0100

    Aktualisierung

---
+++ Functions/Bitlocker/Deploy.cmd
@@ -0,0 +1,17 @@
+@Echo Off
+Rem Kopiert die Files zu den Zielen, wo sie benötigt werden:
+Rem - \\akros.ch\Ablage\IT-Scripts\AD-Bitlocker
+Rem - \\basdc002.akros.ch\FirmaBiel\Infrastruktur\Scripts\BitLocker
+
+SET "ScriptDir=%~dp0"
+
+
+Rem Die Scripts für die IT
+RoboCopy.exe %ScriptDir% "\\akros.ch\Ablage\IT-Scripts\AD-Bitlocker\\" *.cmd *.ps1 /COPY:DT /DCOPY:T /XC /XO /R:3
+
+Rem Die Scripts für die User
+RoboCopy.exe %ScriptDir% "\\basdc002.akros.ch\FirmaBiel\Infrastruktur\Scripts\BitLocker\\" *.cmd *.ps1 /COPY:DT /DCOPY:T /XC /XO /R:3
+
+
+Pause
+
